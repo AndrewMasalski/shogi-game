8e3ef171b8f0e4c74d5bcc516c32f84f2c2b1351
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-11-02 12:38:44 +0000

    Temporary SssWindow has gone: MainWindow is using ShogiBoard now.

--- Yasc/ShogiBoardCore.xaml.cs
+++ Yasc/ShogiBoardCore.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Yasc.GenericDragDrop;
 using Yasc.ShogiCore;
 using Yasc.ShogiCore.Moves;
@@ -18,23 +19,29 @@ namespace Yasc
       get { return (Board) DataContext;}
     }
 
+    public static readonly RoutedEvent MoveAttemptEvent = EventManager.
+      RegisterRoutedEvent("MoveAttempt", RoutingStrategy.Bubble, 
+      typeof(EventHandler<MoveAttemptEventArgs>), typeof(ShogiBoardCore));
+
+    public event EventHandler<MoveAttemptEventArgs> MoveAttempt
+    {
+      add { AddHandler(MoveAttemptEvent, value); }
+      remove { RemoveHandler(MoveAttemptEvent, value); }
+    }
+
+    private void RaiseMoveAttemptEvent(MoveBase move)
+    {
+      RaiseEvent(new MoveAttemptEventArgs(MoveAttemptEvent, this, move));
+    }
+
     private void DropHandler(object sender, DropEventArgs e)
     {
-      MoveBase move = null;
+      MoveBase move;
       if (e.DragSource.DataContext is Cell)
       {
         var from = (Cell)e.DragSource.DataContext;
         var to = (Cell)e.DragTarget.DataContext;
-        var m1 = Board.GetUsualMove(from.Position, to.Position, false);
-        var m2 = Board.GetUsualMove(from.Position, to.Position, true);
-        if (m1.IsValid && m2.IsValid)
-        {
-          var answer = MessageBox.Show("Promote?", "Q",
-            MessageBoxButton.YesNo, MessageBoxImage.Question);
-          move = answer == MessageBoxResult.Yes ? m2 : m1;
-        }
-        else if (m1.IsValid) move = m1;
-        else if (m2.IsValid) move = m2;
+        move = GetUsualMove(from, to);
       }
       else
       {
@@ -43,8 +50,38 @@ namespace Yasc
         move = Board.GetDropMove(piece, to.Position);
       }
 
-      if (move != null && move.IsValid) 
-        Board.MakeMove(move);
+      if (move != null)
+      {
+        RaiseMoveAttemptEvent(move);
+        if (move.IsValid)
+          Board.MakeMove(move);
+      }
+    }
+
+    private MoveBase GetUsualMove(Cell from, Cell to)
+    {
+      MoveBase move;
+      var m1 = Board.GetUsualMove(from.Position, to.Position, false);
+      var m2 = Board.GetUsualMove(from.Position, to.Position, true);
+      if (m1.IsValid && m2.IsValid)
+      {
+        var answer = MessageBox.Show("Promote?", "Q",
+                                     MessageBoxButton.YesNo, MessageBoxImage.Question);
+        move = answer == MessageBoxResult.Yes ? m2 : m1;
+      }
+      else move = m1.IsValid ? m1 : m2;
+      return move;
+    }
+  }
+
+  public class MoveAttemptEventArgs : RoutedEventArgs
+  {
+    public MoveBase Move { get; private set; }
+
+    public MoveAttemptEventArgs(RoutedEvent routedEvent, object source, MoveBase move) 
+      : base(routedEvent, source)
+    {
+      Move = move;
     }
   }
 }
