de00249caed139164e59d6a7f5377c3c9160698a
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-11-16 20:40:00 +0000

    bug 5 is solved. Also possible move cells template now looks 40% cooler!

--- Yasc/Properties/AssemblyInfo.cs
+++ Yasc/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Yasc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Yet another shogi")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Yasc")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright Илья Марголин©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
