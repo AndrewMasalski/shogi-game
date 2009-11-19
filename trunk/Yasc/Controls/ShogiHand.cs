81b6eb0c6b259b261c0e1a847b352c3536b865b1
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-11-19 20:44:56 +0000

    ShogiHand now has Board and Color properties and doesn't work on DataContext

--- trunk/Yasc/Controls/ShogiHand.cs
+++ trunk/Yasc/Controls/ShogiHand.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
+using Yasc.ShogiCore;
+using Yasc.ShogiCore.Utils;
 
 namespace Yasc.Controls
 {
@@ -10,5 +13,56 @@ namespace Yasc.Controls
       DefaultStyleKeyProperty.OverrideMetadata(typeof(ShogiHand),
         new FrameworkPropertyMetadata(typeof(ShogiHand)));
     }
+
+    public static readonly DependencyProperty BoardProperty =
+      DependencyProperty.Register("Board", typeof (Board),
+                                  typeof (ShogiHand), new UIPropertyMetadata(default(Board), OnBoardChanged));
+
+    private static void OnBoardChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      ((ShogiHand)d).OnBoardChanged((Board)e.NewValue);
+    }
+
+    private void OnBoardChanged(Board board)
+    {
+      Hand = board[Color].Hand;
+    }
+
+    public Board Board
+    {
+      get { return (Board) GetValue(BoardProperty); }
+      set { SetValue(BoardProperty, value); }
+    }
+
+    public static readonly DependencyProperty ColorProperty =
+      DependencyProperty.Register("Color", typeof (PieceColor),
+        typeof (ShogiHand), new UIPropertyMetadata(default(PieceColor), OnColorChanged));
+
+    private static void OnColorChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
+    {
+      ((ShogiHand)o).OnColorChanged((PieceColor)args.NewValue);
+    }
+
+    private void OnColorChanged(PieceColor color)
+    {
+      Hand = Board == null ? null : Board[color].Hand;
+    }
+
+    public PieceColor Color
+    {
+      get { return (PieceColor) GetValue(ColorProperty); }
+      set { SetValue(ColorProperty, value); }
+    }
+
+    public static readonly DependencyProperty HandProperty =
+      DependencyProperty.Register("Hand", typeof (ObservableCollection<Piece>),
+                                  typeof (ShogiHand), new UIPropertyMetadata(default(ObservableCollection<Piece>)));
+
+    public ObservableCollection<Piece> Hand
+    {
+      get { return (ObservableCollection<Piece>) GetValue(HandProperty); }
+      set { SetValue(HandProperty, value); }
+    }
+    
   }
 }
