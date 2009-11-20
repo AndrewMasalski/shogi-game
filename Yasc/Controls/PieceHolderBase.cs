e358911947a504a835c2e727618e527dd245478f
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-11-20 17:01:42 +0000

    EffectiveDirection property is introduced for PieceHolderBase

--- Yasc/Controls/PieceHolderBase.cs
+++ Yasc/Controls/PieceHolderBase.cs
@@ -1,12 +1,41 @@
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using Yasc.ShogiCore;
+using Yasc.ShogiCore.Utils;
 
 namespace Yasc.Controls
 {
   [TemplatePart(Name = "PART_Piece", Type = typeof(ContentPresenter))]
   public abstract class PieceHolderBase : Control
   {
-    #region 'IsPossible MoveTarget '
+    #region ' EffectiveDirectionProperty '
+
+    public static readonly DependencyProperty EffectiveDirectionProperty =
+      DependencyProperty.Register("EffectiveDirection", typeof(PieceDirection),
+        typeof(PieceHolderBase), new UIPropertyMetadata(default(PieceDirection)));
+
+    public PieceDirection EffectiveDirection
+    {
+      get { return (PieceDirection)GetValue(EffectiveDirectionProperty); }
+      set { SetValue(EffectiveDirectionProperty, value); }
+    }
+
+    protected static PieceDirection GetEffectiveDirection(bool isFlipped, PieceColor color)
+    {
+      switch (color)
+      {
+        case PieceColor.White:
+          return isFlipped ? PieceDirection.Upwards : PieceDirection.Downwards;
+        case PieceColor.Black:
+          return isFlipped ? PieceDirection.Downwards : PieceDirection.Upwards;
+      }
+      throw new ArgumentOutOfRangeException("color");
+    }
+
+    #endregion
+
+    #region ' IsPossibleMoveTarget Property  '
 
     public static readonly DependencyProperty IsPossibleMoveTargetProperty =
       DependencyProperty.Register("IsPossibleMoveTarget", typeof(bool),
@@ -38,7 +67,18 @@ namespace Yasc.Controls
     #region ' IsFlipped Property '
 
     public static readonly DependencyProperty IsFlippedProperty = ShogiBoard.IsFlippedProperty.AddOwner(
-      typeof(PieceHolderBase), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits));
+      typeof(PieceHolderBase), new FrameworkPropertyMetadata(false, 
+        FrameworkPropertyMetadataOptions.Inherits, OnIsFlippedChanged));
+
+    private static void OnIsFlippedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      ((PieceHolderBase) d).OnIsFlippedChanged((bool) e.NewValue);
+    }
+
+    private void OnIsFlippedChanged(bool value)
+    {
+      EffectiveDirection = GetEffectiveDirection(value, PieceColor);
+    }
 
     public bool IsFlipped
     {
@@ -73,5 +113,53 @@ namespace Yasc.Controls
     {
       get { return Cp != null ? (ShogiPiece)Cp.Content : null; }
     }
+
+
+    #region ' PieceType Property '
+
+    public static readonly DependencyProperty PieceTypeProperty =
+      DependencyProperty.Register("PieceType", typeof(PieceType),
+        typeof(PieceHolderBase), new UIPropertyMetadata(default(PieceType), OnPieceTypeChanged));
+
+    private static void OnPieceTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      ((PieceHolderBase)d).OnPieceTypeChanged((PieceType)e.NewValue);
+    }
+
+    protected virtual void OnPieceTypeChanged(PieceType pieceType)
+    {
+    }
+
+    public PieceType PieceType
+    {
+      get { return (PieceType)GetValue(PieceTypeProperty); }
+      set { SetValue(PieceTypeProperty, value); }
+    }
+
+    #endregion
+
+    #region ' PieceColor Property '
+
+    public static readonly DependencyProperty PieceColorProperty =
+      DependencyProperty.Register("PieceColor", typeof(PieceColor),
+        typeof(PieceHolderBase), new UIPropertyMetadata(default(PieceColor), OnPieceColorChanged));
+
+    private static void OnPieceColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      ((PieceHolderBase)d).OnPieceColorChanged((PieceColor)e.NewValue);
+    }
+
+    protected virtual void OnPieceColorChanged(PieceColor pieceColor)
+    {
+      EffectiveDirection = GetEffectiveDirection(IsFlipped, pieceColor);
+    }
+
+    public PieceColor PieceColor
+    {
+      get { return (PieceColor)GetValue(PieceColorProperty); }
+      set { SetValue(PieceColorProperty, value); }
+    }
+
+    #endregion
   }
 }
