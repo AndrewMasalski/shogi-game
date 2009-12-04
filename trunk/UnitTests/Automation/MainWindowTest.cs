4c4f1debbc6d0017726fc2d421d2b702f6d57eb4
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-12-04 15:20:14 +0000

    Tests Automation started

---
+++ trunk/UnitTests/Automation/MainWindowTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Windows.Automation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Automation
+{
+  [TestClass]
+  public class MainWindowTest
+  {
+    private ApplicationHost _app;
+    private AutomationElement _windowElement;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+      _app = new ApplicationHost();
+      _windowElement = _app.Open();
+    }
+
+    [TestMethod]
+    public void Check()
+    {
+      var pattern = (WindowPattern)_windowElement.
+        GetCurrentPattern(WindowPattern.Pattern);
+      pattern.SetWindowVisualState(WindowVisualState.Maximized);
+      var element = _windowElement.FindFirst(TreeScope.Descendants,
+        new PropertyCondition(AutomationElement.NameProperty, "Play with comp."));
+      var button = (InvokePattern)element.GetCurrentPattern(InvokePattern.Pattern);
+      button.Invoke();
+      var piece = _windowElement.FindFirst(TreeScope.Descendants,
+        new PropertyCondition(AutomationElement.NameProperty, "歩"));
+      Assert.IsNotNull(piece);
+    }
+
+    [TestCleanup]
+    public void TearDown()
+    {
+      _app.Close();
+      _windowElement = null;
+    }
+  }
+}
