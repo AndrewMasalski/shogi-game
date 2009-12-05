a64961af775703af4f340f815022f8f29628fbd1
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-12-05 15:33:43 +0000

    Moves history list box focus cheater

--- Yasc/Gui/GameView.xaml.cs
+++ Yasc/Gui/GameView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Yasc.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Yasc.Controls;
 
 namespace Yasc.Gui
 {
@@ -12,6 +16,50 @@ namespace Yasc.Gui
     private void OnMoveAttempt(object sender, MoveAttemptEventArgs e)
     {
       _errorLabel.Text = e.Move.ErrorMessage;
+    }
+
+    internal class FocusCheater
+    {
+      private readonly ListBox _listBox;
+
+      public FocusCheater(ListBox listBox)
+      {
+        if (listBox == null) throw new ArgumentNullException("listBox");
+        _listBox = listBox;
+        _listBox.PreviewKeyDown += OnPreviewKeyDown;
+      }
+
+      private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+      {
+        int index = _listBox.SelectedIndex;
+        if (e.Key == Key.Left)
+        {
+          if (index > 0 && index % 2 == 0)
+          {
+            var lbi = (ListBoxItem)_listBox.ItemContainerGenerator.ContainerFromIndex(index - 1);
+            lbi.Focus();
+            e.Handled = true;
+          }
+        }
+        else if (e.Key == Key.Right)
+        {
+          if (index < _listBox.Items.Count - 1 && index % 2 == 1)
+          {
+            var lbi = (ListBoxItem)_listBox.ItemContainerGenerator.ContainerFromIndex(index + 1);
+            lbi.Focus();
+            e.Handled = true;
+          }
+          else if (index == _listBox.Items.Count - 1)
+          {
+            e.Handled = true;
+          }
+        }
+      }
+    }
+
+    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+      new FocusCheater(_listBox);
     }
   }
 }
