92ddab0b332482a1a094e74f5ebc148dbb4275c3
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-12-25 20:31:55 +0000

    Implemented issue #18: Stopwatch

---
+++ Yasc/Common/ProgressClock.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Yasc.Common
+{
+  public class ProgressClock : Control
+  {
+    static ProgressClock()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressClock), 
+        new FrameworkPropertyMetadata(typeof(ProgressClock)));
+    }
+
+  }
+}
