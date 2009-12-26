cd5eccaf30097e868889d4ea0941288ded9c4968
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-12-26 20:21:02 +0000

    issue #33 issue #33: Load PSN files plenty of tests pass

--- trunk/UnitTests/Properties/Resources.Designer.cs
+++ trunk/UnitTests/Properties/Resources.Designer.cs
@@ -60,25 +60,10 @@ namespace UnitTests.Properties {
             }
         }
         
-        /// <summary>
-        ///   Looks up a localized string similar to [Date &quot;1993/07/09&quot;]
-        ///[Event &quot;Kisei-sen&quot;]
-        ///[Opening &quot;Yagura&quot;]
-        ///[Black &quot;Habu Yoshiharu&quot;]
-        ///[White &quot;Tanigawa Koji&quot;]
-        ///1.P7f 	2.P8d 	3.G7h 	4.G3b 	5.S6h 	6.P3d 
-        ///7.P6f 	8.S6b 	9.P5f 	10.P5d 	11.S4h 	12.S4b 
-        ///13.G5h 	14.K4a 	15.K6i 	16.G5b 	17.G56g 	18.S3c 
-        ///19.S7g 	20.B3a 	21.B7i 	22.P4d 	23.P3f 	24.P7d 
-        ///25.B6h 	26.G54c 	27.K7i 	28.P8e 	29.R3h 	30.B6d 
-        ///31.N3g 	32.K3a 	33.P2f 	34.K2b 	35.P1f 	36.P1d 
-        ///37.K8h 	38.S5c 	39.L1g 	40.P5e 	41.P6e 	42.B7c 
-        ///43.P5e 	44.P6d 	45.S5g 	46.P6e 	47.S5f 	48.R6b 
-        ///49.N2e 	50.S2d [rest of string was truncated]&quot;;.
-        /// </summary>
-        internal static string Game1_psn {
+        internal static byte[] Jan_Jun1992 {
             get {
-                return ResourceManager.GetString("Game1.psn", resourceCulture);
+                object obj = ResourceManager.GetObject("Jan_Jun1992", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
