fafef116cc3cef82d5f255ce745d2e268b6e4519
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-12-27 10:52:54 +0000

    TeamCity now can set build number in AssemblyInfo.cs

--- trunk/Yasc/Properties/AssemblyInfo.cs
+++ trunk/Yasc/Properties/AssemblyInfo.cs
@@ -43,18 +43,8 @@ using System.Windows;
 )]
 
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.18.0")]
-[assembly: AssemblyFileVersion("0.6.18.0")]
+// No sense to change as this os gonna be altered by builder!
+[assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: InternalsVisibleTo("UnitTests")]
