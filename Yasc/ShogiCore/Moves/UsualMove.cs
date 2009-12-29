a8a6d642f0e146574b22ffcb40ad9b963dbd4331
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2009-12-29 15:15:13 +0000

    issue #10 Done for usual moves

--- Yasc/ShogiCore/Moves/UsualMove.cs
+++ Yasc/ShogiCore/Moves/UsualMove.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using Yasc.ShogiCore.Snapshots;
 
 namespace Yasc.ShogiCore.Moves
 {
   public class UsualMove : MoveBase
   {
+    private string _cuteNotation;
+
     public Position From { get; private set; }
     public Position To { get; private set; }
     public Piece MovingPiece { get; private set; }
@@ -19,6 +25,72 @@ namespace Yasc.ShogiCore.Moves
     {
       return From + "-" + To + (IsPromoting ? "+" : "");
     }
+
+    #region ' CuteNotation '
+
+    public string CuteNotation
+    {
+      get
+      {
+        if (_cuteNotation == null)
+        {
+          _cuteNotation = GetCuteNotation();
+        }
+        return _cuteNotation;
+      }
+    }
+    private string GetCuteNotation()
+    {
+      var sb = new StringBuilder();
+      sb.Append(MovingPiece.PieceType);
+      var hint = GetHint(WhoElseCouldMoveThere());
+      if (hint != null)
+      {
+        sb.Append(hint);
+        if (TakenPiece == null)
+          sb.Append("-");
+      }
+      if (TakenPiece != null)
+        sb.Append("x");
+      sb.Append(To);
+      return sb.ToString();
+    }
+    private string GetHint(IEnumerable<Position> list)
+    {
+      var s = new HashSet<char>(From.ToString());
+      foreach (var position in list)
+        foreach (var c in position.ToString())
+          s.Remove(c);
+
+      if (s.Count == 0)
+      {
+        return From.ToString();
+      }
+      if (s.Count == 1)
+      {
+        return s.First().ToString();
+      }
+      return null;
+    }
+    private IEnumerable<Position> WhoElseCouldMoveThere()
+    {
+      /*  return from p in Position.OnBoard
+               where p != From &&
+                     Board[p] != null &&
+                     Board[p].PieceType == MovingPiece.PieceType &&
+                     Board[p].Color == Who.Color &&
+                     Board.GetAvailableMoves(p).Count(m => m.To == To) > 0
+               select p;*/
+      foreach (var p in Position.OnBoard)
+        if (p != From)
+          if (BoardSnapshot[p] != null)
+            if (BoardSnapshot[p].PieceType == MovingPiece.PieceType)
+              if (BoardSnapshot[p].Color == Who.Color)
+                if (BoardSnapshot.GetAvailableUsualMoves(p).Count(m => m.To == To) > 0)
+                  yield return p;
+    }
+
+    #endregion
 
     #region ' Construction '
 
