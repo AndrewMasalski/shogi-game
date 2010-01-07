10212d66841e4d6197efd23e4c9f248f0fb85a90
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2010-01-07 18:26:42 +0000

    Repository infrastructure is totally

--- DotUsi/DotUsi/Exceptions/CouldntStartProcessException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace DotUsi
-{
-  /// <summary>Represents error that occurs if engine process couldn't start for any reason</summary>
-  internal class CouldntStartProcessException : Exception
-  {
-  }
-}
