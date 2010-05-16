35f9315c5266c301b5ad3073624703452498c92d
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2010-05-16 18:21:19 +0000

    Refactor Cell.cs to not depend on Board

--- trunk/ShogiCore/UnitTests/ShogiCore/PieceTest.cs
+++ trunk/ShogiCore/UnitTests/ShogiCore/PieceTest.cs
@@ -23,54 +23,54 @@ namespace ShogiCore.UnitTests.ShogiCore
       var b = new Board();
       var c = b[0, 0];
       Assert.IsNull(c.Piece);
-      c.ResetPiece();
+      b.ResetPiece(c.Position);
       Assert.IsNull(c.Piece);
       var piece = b.PieceSet["馬"];
-      c.SetPiece(piece, b.White);
+      b.SetPiece(c.Position, piece, b.White);
       Assert.AreSame(c.Piece, piece);
-      c.SetPiece(piece);
+      b.SetPiece(c.Position, piece);
       Assert.AreSame(c.Piece, piece);
     }
     [TestMethod, ExpectedException(typeof(ArgumentNullException))]
     public void SetNullOwnerCellTest()
     {
       var b = new Board();
-      b[0, 0].SetPiece(b.PieceSet["馬"], null);
+      b.SetPiece(b[0, 0].Position, b.PieceSet["馬"], null);
     }
     [TestMethod, ExpectedException(typeof(ArgumentNullException))]
     public void SetNullPieceCellTest()
     {
       var b = new Board();
-      b[0, 0].SetPiece(null, b.White);
+      b.SetPiece(b[0, 0].Position, null, b.White);
     }
     [TestMethod, ExpectedException(typeof(InvalidOperationException))]
     public void SetPieceWhichIsAlreadySet()
     {
       var b = new Board();
       var piece = b.PieceSet["馬"];
-      b[0, 0].SetPiece(piece, b.White);
-      b[0, 1].SetPiece(piece, b.White);
+      b.SetPiece(b[0, 0].Position, piece, b.White);
+      b.SetPiece(b[0, 1].Position, piece, b.White);
     }
     [TestMethod, ExpectedException(typeof(ArgumentNullException))]
     public void SetNullPieceTest()
     {
       var b = new Board();
-      b[0, 0].SetPiece(null);
+      b.SetPiece(b[0, 0].Position, null);
     }
     [TestMethod]
     public void SetPieceWithOwner()
     {
       var b = new Board();
       var piece = b.PieceSet["馬"];
-      b[0, 0].SetPiece(piece, b.White);
-      b[0, 1].SetPiece(piece);
+      b.SetPiece(b[0, 0].Position, piece, b.White);
+      b.SetPiece(b[0, 1].Position, piece);
     }
     [TestMethod, ExpectedException(typeof(PieceHasNoOwnerException))]
     public void SetOwnerlessPiece()
     {
       var b = new Board();
       var piece = b.PieceSet["馬"];
-      b[0, 0].SetPiece(piece);
+      b.SetPiece(b[0, 0].Position, piece);
     }
   }
 }
