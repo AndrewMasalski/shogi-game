0a63ea288932849352d31b46d77ae9f3582f5b3a
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2010-06-03 09:16:29 +0000

    PSN struggle continues

---
+++ trunk/BoardControl/BoardViewer/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Controls;
+using Yasc.ShogiCore.Core;
+using Yasc.ShogiCore.PieceSets;
+using Yasc.ShogiCore.Snapshots;
+
+namespace Yasc.BoardViewer
+{
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      _board.Board = new Board(new StandardPieceSet());
+    }
+
+    private void SfenChanged(object sender, TextChangedEventArgs e)
+    {
+      try
+      {
+        _board.Board.LoadSnapshotWithoutHistory(BoardSnapshot.ParseSfen(_sfen.Text));
+      }
+      catch (ArgumentOutOfRangeException x)
+      {
+        Console.WriteLine(x);
+      }
+      catch (NotEnoughPiecesInSetException x)
+      {
+        Console.WriteLine(x);
+      }
+      catch (InvalidOperationException x)
+      {
+        Console.WriteLine(x);
+      }
+    }
+  }
+}
