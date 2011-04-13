7fe807c913d2a661385c40bca19cca64e6b340a1
marhoily <marhoily@dff2c6a6-bc37-11de-bda7-396a5af1c03d>
2011-04-13 09:52:54 +0000

    24 tests pass

--- trunk/Chess/UniTests/BoardPanelTest.cs
+++ trunk/Chess/UniTests/BoardPanelTest.cs
@@ -63,12 +63,69 @@ namespace UniTests
     [TestMethod]
     public void LayoutColumn()
     {
-      SetupObject(new Vector(3, -1), new Size(16, 88), new Size(16, 88), 
+      SetupObject(new Vector(3, -1), new Size(16, 88), 
         new Rect((16*9 - 16)/8*3, 0, 16, 88));
       _panel.Measure(new Size(16 * 9, 88));
       _panel.Arrange(new Rect(1, 3, 16 * 9, 88));
       Verify();
     }
+    [TestMethod]
+    public void LayoutRow()
+    {
+      SetupObject(new Vector(-1, 4), new Size(80, 7), 
+        new Rect(0,(7*9 - 7)/8*4, 80, 7));
+      _panel.Measure(new Size(80, 7*9));
+      _panel.Arrange(new Rect(1, 3, 80, 7*9));
+      Verify();
+    }
+    [TestMethod]
+    public void LayoutTopLeftCorner()
+    {
+      // We do not expect to get any space arranged as
+      // by now corners are given no more size than edges
+      SetupObject(new Vector(0, 0), new Size(0, 0), 
+        new Size(7, 5), new Rect(0, 0, 0, 0));
+      _panel.Measure(new Size(80, 88));
+      _panel.Arrange(new Rect(1, 3, 80, 88));
+      Verify();
+    }
+    [TestMethod]
+    public void LayoutBottomLeftCorner()
+    {
+      // We do not expect to get any space arranged as
+      // by now corners are given no more size than edges
+      SetupObject(new Vector(0, 9), new Size(0, 0), 
+        new Size(7, 5), new Rect(0, 88, 0, 0));
+      _panel.Measure(new Size(80, 88));
+      _panel.Arrange(new Rect(1, 3, 80, 88));
+      Verify();
+    }
+    [TestMethod]
+    public void LayoutTopRightCorner()
+    {
+      // We do not expect to get any space arranged as
+      // by now corners are given no more size than edges
+      SetupObject(new Vector(9, 0), new Size(0, 0), 
+        new Size(7, 5), new Rect(80, 0, 0, 0));
+      _panel.Measure(new Size(80, 88));
+      _panel.Arrange(new Rect(1, 3, 80, 88));
+      Verify();
+    }
+    [TestMethod]
+    public void LayoutBottomRightCorner()
+    {
+      // We do not expect to get any space arranged as
+      // by now corners are given no more size than edges
+      SetupObject(new Vector(9, 9), new Size(0, 0), 
+        new Size(7, 5), new Rect(80, 88, 0, 0));
+      _panel.Measure(new Size(80, 88));
+      _panel.Arrange(new Rect(1, 3, 80, 88));
+      Verify();
+    }
+
+
+    #region ' Implementation '
+
     private void SetupObject(Vector pos, Size expectedMeasuredSize, Rect expectedArrangeRect)
     {
       SetupObject(pos, expectedMeasuredSize, expectedMeasuredSize, expectedArrangeRect);
@@ -93,5 +150,7 @@ namespace UniTests
     {
       _mocks.ForEach(m => m.Verify());
     }
+
+    #endregion
   }
 }
